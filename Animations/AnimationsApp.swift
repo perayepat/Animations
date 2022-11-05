826d6a8e775cde763bcef736b3bb84a6e622b76c
Mphathi Maapola <113903905+Patglu@users.noreply.github.com>
2022-11-05 12:15:25 +0200

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Pat on 2022/11/05.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
