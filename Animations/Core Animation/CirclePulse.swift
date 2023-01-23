81ca3b47469bc81d47433199541bcaac534e662d
Mphathi Maapola <113903905+Patglu@users.noreply.github.com>
2023-01-23 06:56:07 +0200

    Animating Circles

---
+++ Animations/Core Animation/CirclePulse.swift
@@ -0,0 +1,37 @@
+import SwiftUI
+
+struct CirclePulse: View {
+    
+    @State private var animatingSmallCircle = false
+    @State private var animatingLargeCircle = false
+    
+    var body: some View {
+        ZStack {
+            Circle()
+                .stroke()
+                .frame(width: UIScreen.main.bounds.width - 50, height: UIScreen.main.bounds.width - 50, alignment: .center)
+                .scaleEffect(animatingLargeCircle ? 1.2 : 0.73)
+                .opacity(animatingLargeCircle ? 0 : 1)
+                .animation(.easeInOut(duration: 4).delay(1).repeatForever(autoreverses: false), value: animatingLargeCircle)
+                .onAppear {
+                    animatingLargeCircle.toggle()
+                }
+            
+            Circle()
+                .stroke()
+                .frame(width: UIScreen.main.bounds.width - 100, height: UIScreen.main.bounds.width - 100, alignment: .center)
+                .scaleEffect(animatingSmallCircle ? 1.2 : 0.84)
+                .opacity(animatingSmallCircle ? 0 : 1)
+                .animation(.easeInOut(duration: 4).delay(1).repeatForever(autoreverses: false), value: animatingSmallCircle)
+                .onAppear {
+                    animatingSmallCircle.toggle()
+                }
+        }
+    }
+}
+
+struct CirclePulse_Previews: PreviewProvider {
+    static var previews: some View {
+        CirclePulse()
+    }
+}
